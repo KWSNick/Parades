b1946473cc85cdfdf7f85d899f540a552bbdb007
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-06-29 16:35:06 +0000

    added in bootstrap, hover css and jquery. Altered header to bootstrap navbar and applied classes with styline via jquery.

---
+++ assets/js/classes.js
@@ -0,0 +1,11 @@
+$("document").ready(function(){
+    $(".container-fluid").addClass("noPadding");
+    $("#header").addClass("col-12 noPadding navbar navbar-expand-xl navbar-light bgHeader");    
+    $(".navbar-brand").addClass("col-2 noPadding");
+    $("#tagline").addClass("col-2");
+    $("#tagline h1, #headerContact h1").addClass("d-block taglinePadding");
+    $(".navbar-nav").addClass("col-4 height100 noPadding");
+    $(".nav-item").addClass("col-6 noPadding");
+    $(".nav-link").addClass("height100 width100 noPadding paddingTopBottom12 navLinkText hvr-sweep-to-bottom");
+    $("#headerContact").addClass("col-4");
+})
