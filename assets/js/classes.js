7ea6513d1c776cf8652fdd08fd4c086df3bca3b0
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-06-30 09:36:08 +0000

    header is now responsive

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -2,10 +2,12 @@ $("document").ready(function(){
     $(".container-fluid").addClass("noPadding");
     $("#header").addClass("col-12 noPadding navbar navbar-expand-xl navbar-light bgHeader");    
     $(".navbar-brand").addClass("col-2 noPadding");
-    $("#tagline").addClass("col-2 navBorderLeft navBorderRight");
+    $("#tagline").addClass("d-none d-xl-block col-2 navBorderLeft navBorderRight");
     $("#tagline h1, #headerContact h1").addClass("d-block taglinePadding");
-    $(".navbar-nav").addClass("col-4 height100 noPadding");
-    $(".nav-item").addClass("col-6 noPadding");
+    $("#hamburger").addClass("navbar-toggler ml-auto noBorder");
+    $("#navbarNavigation").addClass("col height100 noPadding collapse navbar-collapse");
+    $(".navbar-nav").addClass("col noPadding ml-auto");
+    $(".nav-item").addClass("col col-xl-6 noPadding");
     $(".nav-link").addClass("height100 width100 noPadding paddingTopBottom12 navLinkText hvr-sweep-to-bottom");
-    $("#headerContact").addClass("col-4 navBorderLeft");
+    $("#headerContact").addClass("d-none d-xl-block col-4 navBorderLeft");
 })
