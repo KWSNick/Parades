bc4e3b14651cad573a93418315e1c3eb925b88e2
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-07-14 12:40:50 +0000

    removed mainFont application from all sub elements and applied it directly to the body element

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,15 +1,15 @@
 $("document").ready(function() {
-    $("body").addClass("bgGreenFibers");
+    $("body").addClass("bgGreenFibers mainFont");
     $("article").addClass("bgGlass  articleMargin");
     $("h3").addClass("subHeaderFont");
     $(".container-fluid").addClass("noPadding");
     $("#header").addClass("col-12 noPadding navbar navbar-expand-xl navbar-light bgHeader");    
     $(".navbar-brand").addClass("col-6 col-xl-2 noPadding noMargin textAlignLeft");
     $("#tagline").addClass("d-none d-xl-block col-2 navBorderLeft navBorderRight");
-    $("#tagline h1, #headerContact h1").addClass("d-block mainFont taglinePadding");
+    $("#tagline h1, #headerContact h1").addClass("d-block taglinePadding");
     $("#hamburger").addClass("navbar-toggler ml-auto noBorder col-2 col-md-1 maxWidth100");
     $("#navbarNavigation").addClass("col height100 noPadding collapse navbar-collapse");
-    $(".navbar-nav").addClass("col noPadding mainFont ml-auto");
+    $(".navbar-nav").addClass("col noPadding ml-auto");
     $(".nav-item").addClass("col col-xl-6 noPadding");
     $(".nav-link").addClass("height100 width100 noPadding paddingTopBottom12 navLinkText hvr-sweep-to-bottom");
     $("#headerContact").addClass("d-none d-xl-block col-4 navBorderLeft");
@@ -22,7 +22,7 @@ $("document").ready(function() {
     $("#innerMainFooterNav>ul, #innerMainFooterNote>p, #innerMainFooterCopy>p").addClass("noMargin");
     $("#innerMainFooterNav>ul").addClass("d-flex flex-row noPadding noListStyle");
     $("#innerMainFooterNav>ul>li").addClass("ml-auto mr-auto");
-    $("#intro").addClass("col-12 font1 mainFont noPadding");
+    $("#intro").addClass("col-12 font1 noPadding");
     $("#innerContents").addClass("col-8");
     $("#sidebar").addClass("col-4");
 })
