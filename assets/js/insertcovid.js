d2d6a6235912b7fbdd0e31eab87c26d294d7bf06
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-07-15 13:33:37 +0000

    Added delayed slideDown function and onclick slide up function to #covid

--- assets/js/insertcovid.js
+++ assets/js/insertcovid.js
@@ -1,3 +1,13 @@
 $("document").ready(function(){
-    $("div").first().before('<div class="container-fluid"><aside id="covid"><strong>COVID-19:</strong> We are fully open and have implemented procedures to minimise risk.  <a href="#">Learn More</a></aside></div>');
+    $("div").first().before('<div class="container-fluid"><aside id="covid"><strong>COVID-19:</strong> We are fully open and have implemented procedures to minimise risk.  <a href="#">Learn More</a> <button type="button" class="close" data-dismiss="covid" aria-label="close"><span aria-hidden="true">&times;</span></button></button></aside></div>');
+    $("#covid").hide();
+    
+    function covidSlide() {
+    $("#covid").delay(3000).slideDown(500);
+};
+    covidSlide();
+
+    $("#covid>button").click(function(){
+        $("#covid").slideUp(500);
+    });
 })
