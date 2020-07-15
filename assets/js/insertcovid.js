ddefb2ffc33d86371d8bd9ae57b4387fd885cdee
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-07-15 15:39:36 +0000

    altered content of modal body

--- assets/js/insertcovid.js
+++ assets/js/insertcovid.js
@@ -3,7 +3,7 @@ $("document").ready(function(){
     $("#covid").hide();
     $("body").append(`<div id="covidModal"><div class="modal-dialog modal-lg modal-dialog-centered"><div class="modal-content"><div class="modal-header"></div><div class="modal-body"></div><div class="modal-footer"></div></div></div></div>`);
     $("#covidModal>div>div>.modal-header").append(`<h4>COVID 19 risk management at Parades</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>`);
-    $("#covidModal>div>div>.modal-body").append(`<ul><li>No need to book an appointment.</li><li>We are limiting customer numbers in store, and may ask you to wait outside.</li><li>Hand sanitiser is available for staff and customers.</li><li>Surfaces and touch points disinfected regularly.</li><li>Our staff will be wearing masks in store.</li></ul>`);
+    $("#covidModal>div>div>.modal-body").append(`<ul><li>No need to book an appointment.</li><li>We are limiting customer numbers in store, and may ask you to briefly wait outside.</li><li>Hand sanitiser is available for staff and customers.</li><li>Surfaces and touch points disinfected regularly.</li><li>Our staff will be wearing masks in store.</li></ul>`);
     $("#covidModal>div>div>.modal-footer").append(`<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>`);
     function covidSlide() {
     $("#covid").delay(3000).slideDown(500);
