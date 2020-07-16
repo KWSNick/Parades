f862f961028c39b08a0425181dd51f08df0e55ba
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-07-16 12:51:56 +0000

    added .card-body and a recipient of the rangesizing.js function

--- assets/js/rangessizing.js
+++ assets/js/rangessizing.js
@@ -1,8 +1,10 @@
 $("document").ready(function () {
   let varHeight = $(".card").width();
   $(".card").css({ height: varHeight });
+  $(".card-body").css({ height: varHeight });
   $(window).resize(function () {
     let varHeight = $(".card").width();
     $(".card").css({ height: varHeight });
+    $(".card-body").css({ height: varHeight });
   });
 });
