62687312d7227900d2ea93594fe49fdd821993f4
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-07-16 12:35:44 +0000

    added JQuery resize function on window size change

--- assets/js/rangessizing.js
+++ assets/js/rangessizing.js
@@ -1,5 +1,8 @@
 $("document").ready(function () {
   let varHeight = $(".card").width();
-
   $(".card").css({ height: varHeight });
+  $(window).resize(function () {
+    let varHeight = $(".card").width();
+    $(".card").css({ height: varHeight });
+  });
 });
