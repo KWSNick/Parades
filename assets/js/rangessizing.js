92932afc78d53f3cb4526afbeaf58ffdd408a154
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-07-16 12:54:23 +0000

    removed separate line for application of css height to card-body in rangesizing.js and included in existing script for .card

--- assets/js/rangessizing.js
+++ assets/js/rangessizing.js
@@ -1,10 +1,8 @@
 $("document").ready(function () {
   let varHeight = $(".card").width();
-  $(".card").css({ height: varHeight });
-  $(".card-body").css({ height: varHeight });
+  $(".card, .card-body").css({ height: varHeight });
   $(window).resize(function () {
     let varHeight = $(".card").width();
-    $(".card").css({ height: varHeight });
-    $(".card-body").css({ height: varHeight });
+    $(".card, .card-body").css({ height: varHeight });
   });
 });
