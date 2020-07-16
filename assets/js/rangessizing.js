b8552725584faad54b7e87d517f27966c638e942
Nicholas Bowley <nicholasbowley@hotmail.com>
2020-07-16 13:35:12 +0000

    declared headerheight variable which gets height of card-header elements within rangesizing.js. Removed card-body from card height calculation and created a new calculation which defines card-body height based on total card height minus header height

--- assets/js/rangessizing.js
+++ assets/js/rangessizing.js
@@ -1,8 +1,11 @@
 $("document").ready(function () {
   let varHeight = $(".card").width();
-  $(".card, .card-body").css({ height: varHeight });
+  let headerHeight = $(".card-header").height();
+  $(".card").css({ height: varHeight });
+  $(".card-body").css({ height: varHeight - headerHeight });
   $(window).resize(function () {
     let varHeight = $(".card").width();
     $(".card, .card-body").css({ height: varHeight });
+    $(".card-body").css({ height: varHeight - headerHeight });
   });
 });
